a669bdb62b3ba42cbdf53843115f9cb10a6cf791
IST440W <none@psu.edu>
2019-10-16 00:38:00 +0000

    Initial commit of decoder flask

---
+++ app.py
@@ -0,0 +1,57 @@
+
+# A very simple Flask Hello World app for you to get started with...
+# http://flask.palletsprojects.com/en/1.0.x/patterns/fileuploads/
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
+
+from werkzeug.utils import secure_filename
+VERSION="1.2"
+
+UPLOAD_FOLDER = '/opt/ist440/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['VERSION'] = VERSION
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', title='Cracking The Code', version=VERSION)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload_page():
+    header = "<h1>Upload</h1>"
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print app.config['UPLOAD_FOLDER']
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    else:
+
+        body = """<p>Upload an image:</p> <form method=post enctype=multipart/form-data>
+  <input type="file" name="file">
+  <input type="submit" value="Upload">
+</form>
+"""
+        footer = ""
+    return header + body + footer
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=3000)
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+#!/opt/ist440/venv/bin/python
+from app import app
+if __name__ == "__main__":
+    app.run()
